c3289910d7e5a1f6583b3df4efaeaae1accb793e
sushant <bksushantofficial@gmail.com>
2026-05-26 22:33:03 +0545

    event handling in react and used sweet alert 2 library to make it more beautiful

--- reacting/src/components/Card.jsx
+++ reacting/src/components/Card.jsx
@@ -1,50 +1,45 @@
 import React from 'react'
-
+import Swal from "sweetalert2";
 const Card = () => {
+   
+  const data = [
+      {name:"i'll be missing you", description:"“I’ll Be Missing You” is a heartfelt tribute song about grief, love, and remembering a lost friend."},
+      {name:"Cupid fifty fifty", description:"“Cupid” by FIFTY FIFTY is a light, catchy song about longing for love and hoping Cupid helps find the right person."},
+      {name:"renai circulation", description:"“Renai Circulation” is a cute, upbeat song about having a shy crush and slowly realizing feelings of love with a playful and cheerful tone. "}  
 
-   const data = [{
-    image:'https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1196&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', name:"amazon basics ", description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae dolor at consequatur.",
-    instock:true
-   },
-  {
-    image:'https://plus.unsplash.com/premium_photo-1677706562562-5aa4caaa2fd4?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name:"Daily objects",
-    description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae dolor at consequatur. ",
-    instock:false
-  },
+  ]
 
-   {
-    image:'https://images.unsplash.com/photo-1585184394271-4c0a47dc59c9?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name:"apple",
-    description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae dolor at consequatur.Apple company a trillion dollar company ",
-    instock:false
-  },
+  // const handleClickDownload =()=>{
+  //   alert("Downloading...")
+  // }
 
-]
+
+
+  
+const handleClickDownload = () => {
+  Swal.fire({
+    title: "Downloading...",
+    text: "Please wait while your file is being prepared 🎵",
+    icon: "info",
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+  });
+};
 
   return (
-    <div  className='w-full h-screen bg-zinc-200  flex items-center justify-center  gap-10'>
-       {data.map((elem,index)=>(
-         <div key={index} className='w-52 bg-zinc-100  rounded-r-md overflow-hidden'>
-        <div className='w-full h-32 bg-zinc-300 '>
-            <img className='w-full h-full object-cover' src={elem.image} alt="amzonImageBox" />
-        </div>
-        <div className='w-full px-3 py-4'>
-                    <h2 className='font-semibold'>{elem.name}</h2>
-        <p className='text-sm mt-5'>{elem.description}</p>
-        <button className={`px-4 py-1  text-xs mt-3 rounded text-zinc-100 ${elem.instock?'bg-blue-600':'bg-red-600'} `}>
-          {elem.instock? "Instock":"out of stock"}
-        </button>
-        
-        </div>
+    <div className='w-full h-screen bg-zinc-300 flex flex-col gap-4 items-center justify-center'>
+         {data.map((item,index)=>(
+          
+      <div className=' w-90 px-3 py-2 bg-zinc-100 rounded-md'>
+        <h3 className='font-semibold text-xl'>{item.name}</h3>
+        <p className='text-sm mt-2'>{item.description}</p>
 
-        </div>
-       ))}
+        <button onClick={handleClickDownload} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-100 rounded mt-3'> Download Now</button>
+         </div>
+         ))}
     </div>
   )
 }
 
 export default Card
-
-
-
